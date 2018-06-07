da724385fbc3cace1f486fc48a526d5b15d8e696
Andre Franklin <Andre Franklin@192.168.0.7>
2018-06-07 19:48:43 -0300

    Criado pacote de conexao e alteraçoes no form login

---
+++ src/java/Conexao/ConexaoBD.java
@@ -0,0 +1,30 @@
+package Conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoBD {
+
+	private static final String USUARIO="root";
+	private static final String SENHA="";
+	private static final String URL = "jdbc:mysql://localhost:3306/banco de dados";
+	
+	public static Connection conectar() throws SQLException {
+		
+		Connection conexao = DriverManager.getConnection(URL,USUARIO,SENHA);
+		return conexao;		
+	}
+	
+	public static void main(String[] args) {
+		try {
+			Connection conexao = ConexaoBD.conectar();
+			System.out.println("Banco de dados conectado com sucesso!");
+		}
+		catch(SQLException ex) {
+			ex.printStackTrace();
+			System.out.println("Conexão com o Banco de dados falhou !");
+		}
+        }
+}
+	
